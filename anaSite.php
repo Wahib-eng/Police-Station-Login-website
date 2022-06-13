865932ca15fc80595d3c28c53da9cdca933ca89c
WAHIB MOQBEL <75446834+Wahib-eng@users.noreply.github.com>
2022-06-13 09:13:33 +0300

    Create anaSite.php

---
+++ anaSite.php
@@ -0,0 +1,166 @@
+<?php
+  
+ 
+   
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <title>police station</title>
+		    <meta charset="utf-8">
+        <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+        <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+        <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+        <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+        <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+        <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+        <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+      
+  <link href="assets/css/style.css" rel="stylesheet">
+    </head>
+    <body>
+  <header id="header" class="fixed-top header-inner-pages">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html">Police Station</a></h1>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto " href="anaSite.php">Home </a></li>
+          <li><a class="nav-link scrollto" href="">INFORMATION</a></li>
+          <li><a class="nav-link scrollto" href="">COMMUNITY</a></li>
+          <li><a class="nav-link   scrollto" href="">ADVISORIES</a></li>
+          <li><a class="nav-link scrollto" href="">EVERTS</a></li>
+          <li class="dropdown"><a href=""><span>Police stations in the city</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Ankara</a></li>
+              <li class="dropdown"><a href="#"><span>Bursa</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">yildirim  1</a></li>
+                  <li><a href="#">osmangazi</a></li>
+                  <li><a href="#">shrekustu</a></li>
+                  <li><a href="#">gorukle</a></li>
+                  <li><a href="#">batikent</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Istanbul</a></li>
+              <li><a href="#">Nefshir</a></li>
+              <li><a href="#">Izmir </a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="YeniGiris.php">sign up </a></li>
+          <li><a class="getstarted scrollto" href="oturum.php">sign in </a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+
+    </div>
+  </header>
+
+  <main id="main">
+
+
+<section id="breadcrumbs" class="breadcrumbs">
+  
+</section>
+</main>
+<footer id="footer">
+<div class="footer-newsletter">
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-6">
+        <h4>Join Our police officers </h4>
+        <p>The last registeration date is 11/10/2022 take addventage to be the one who saves people</p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="footer-top">
+  <div class="container">
+    <div class="row">
+
+      <div class="col-lg-3 col-md-6 footer-contact">
+        <h3>Police Station </h3>
+        <p>
+          A108 Adam Street <br>
+          New York, NY 535022<br>
+          United States <br><br>
+          <strong>Phone:</strong> +537 089 5588<br>
+          <strong>Email:</strong> policeStation@force.com<br>
+        </p>
+      </div>
+
+      <div class="col-lg-3 col-md-6 footer-links">
+        <h4>Useful Links</h4>
+        <ul>
+          <li><i class="bx bx-chevron-right"></i> <a href="anaSite.php">Home</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">National Service</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Police Day</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Police Heritage Trail</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Our History</a></li>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 footer-links">
+        <h4>Our Services</h4>
+        <ul>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Airport Security</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Traffic</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Civilian officer</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Crime</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Volunteer Schemes</a></li>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 footer-links">
+        <h4>Our Social Networks</h4>
+        <p>We are here to help you. We protect you anywhere in the city </p>
+        <div class="social-links mt-3">
+          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<div class="container footer-bottom clearfix">
+  <div class="copyright">
+    &copy; Copyright <strong><span>Police Station</span></strong>. All Rights Reserved
+  </div>
+  <div class="credits">
+   
+  </div>
+</div>
+</footer>
+
+<div id="preloader"></div>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+
+<script src="assets/vendor/aos/aos.js"></script>
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+<script src="assets/js/main.js"></script>
+
+
+    </body> 
+</html>
