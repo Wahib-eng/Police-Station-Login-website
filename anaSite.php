71acd064bb025a7cbb5b2766efbe789b1e93bff4
WAHIB MOQBEL <75446834+Wahib-eng@users.noreply.github.com>
2022-06-17 15:42:11 +0300

    Update anaSite.php

--- anaSite.php
+++ anaSite.php
@@ -30,7 +30,8 @@
         <title>police station</title>
     </head>
     <body>
-      <div>
+    <? echo $_SESSION['polis_id'] ?>
+  
     
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
   <div class="container-fluid">
@@ -59,11 +60,6 @@
         <li class="nav-item">
           <a class="btn btn-danger" href="cikis.php">log out  </a>
         </li>
-        
-        
-        
-        
-        
       </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="text" placeholder="Search">
@@ -72,91 +68,73 @@
     </div>
   </div>
 </nav> 
-</div>
 
-<main id="main">
-<section id="breadcrumbs" class="breadcrumbs">
-  
-</section>
-</main>
-<footer id="footer">
-<div class="footer-newsletter">
+
   <div class="container">
-    <div class="row justify-content-center">
-      <div class="col-lg-6">
-        <h4>Join Our police officers </h4>
-        <p>The last registeration date is 11/10/2022 take addventage to be the one who saves people</p>
+    <div class="row justify-content-left">
+      <div class="col-lg-6 col-md-6 col-12"><br><br>
         
+        <h2 style="color: grey; "> Update Page </h2> <br><br>
+           <div class="mb-3">
+                <label class="form-label">Name</label>
+                <input type="text" class="form-control"  name="polis_adi">
+           </div>
+           <div class="mb-3">
+               <label  class="form-label">Date of Birth</label>
+               <input type="date" class="form-control"  name="polis_dt">
+           </div>
+           <div class="mb-3">
+               <label  class="form-label">Email</label>
+               <input type="email" class="form-control"  placeholder="username@gmail.com"  name="polis_eposta">
+           </div>
+           <div class="mb-3">
+               <label  class="form-label">Password</label>
+               <input type="password" class="form-control" name="sifre"><br>
+           </div>
+           <div class="mb-3">
+               <label  class="form-label">Phone No</label>
+           <input type="text" class="form-control"  placeholder="0533-002-22-22"  name="polis_tn">
+           </div>
+           <div class="mb-3">
+               <label class="form-label">Address</label>
+               <input type="text" class="form-control"  name="polis_adresi">
+           </div>
+           
+           <div class="mb-3">
+                <button type="submit" class="btn btn-success">update</button> <br><br>
+           </div>
+       </form>
       </div>
     </div>
   </div>
-</div><br><br><br><br><br><br><br><br><br>
 
-<div class="footer-top">
   <div class="container">
-    <div class="row">
+    <div class="row justify-content-right">
+      <div class="col-lg-6 col-md-6 col-12">
 
-      <div class="col-lg-3 col-md-6 footer-contact">
-        <h3>Police Station </h3>
-        <p>
-          A108 Adam Street <br>
-          New York, NY 535022<br>
-          United States <br><br>
-          <strong>Phone:</strong> +537 089 5588<br>
-          <strong>Email:</strong> policeStation@force.com<br>
-        </p>
-      </div>
+    <form action="<?php $_PHP_SELF ?>" method="post" class="container" style="margin-top :50px; margin-bottom:50px">
+          <?php  
+            if(isset($mesaj)){ echo $mesaj;}  
+            
+            ?>
+            <h2 style="color: grey; "> Delete Account  </h2><br> <br> 
+       <div class="mb-3">
+          <label class="form-label">Username  </label>
+          <input type="text" class="form-control" name="polis_adi" >
+        </div>  
+        <div class="mb-3">
+           <label class="form-label">Password  </label>
+           <input name="password" type="password"   class="form-control" >
+        </div>  
 
-      <div class="col-lg-3 col-md-6 footer-links">
-        <h4>Useful Links</h4>
-        <ul>
-          <li><i class="bx bx-chevron-right"></i> <a href="anaSite.php">Home</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">National Service</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Police Day</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Police Heritage Trail</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Our History</a></li>
-        </ul>
-      </div>
-
-      <div class="col-lg-3 col-md-6 footer-links">
-        <h4>Our Services</h4>
-        <ul>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Airport Security</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Traffic</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Civilian officer</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Crime</a></li>
-          <li><i class="bx bx-chevron-right"></i> <a href="#">Volunteer Schemes</a></li>
-        </ul>
-      </div>
-
-      <div class="col-lg-3 col-md-6 footer-links">
-        <h4>Our Social Networks</h4>
-        <p>We are here to help you. We protect you anywhere in the city </p>
-        <div class="social-links mt-3">
-          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-          <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-        </div>
-      </div>
-
+       <div>
+          <button  type="submit" class="btn btn-success"> Delete  </button>
+       </div><br>
+     <div >
+     </form>
+     </div>
     </div>
   </div>
-</div>
-
-<div class="container footer-bottom clearfix">
-  <div class="copyright">
-    &copy; Copyright <strong><span>Police Station</span></strong>. All Rights Reserved
-  </div>
-  <div class="credits">
-   
-  </div>
-</div>
-</footer>
-
-<div id="preloader"></div>
-<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
     </body> 
 </html>
