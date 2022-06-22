6a389b9626c1200839e616cb019a74cf4eeaa623
WAHIB MOQBEL <75446834+Wahib-eng@users.noreply.github.com>
2022-06-22 09:31:28 +0300

    Update giris.php

--- giris.php
+++ giris.php
@@ -1,72 +1,96 @@
-<!DOCTYPE html>
-<?php
-    session_start();
-    require('mysqlConn.php');
-    if (isset($_POST['polis_adi']) and isset($_POST['password'])){
-    extract($_POST);
+<?php 
+   session_start(); 
    
-    $password = hash('sha256', $password);
-    $sql = "SELECT * FROM `polis` WHERE";
-    $sql= $sql . "kullaniciadi='$polis_adi' and sifre='$password'";
+   require('MysqlConn.php'); 
+   
     
-    $answer = mysqli_query($connect, $sql);
-    
-    if(!$answer){
-    echo '<br>Error:' . mysqli_error($connect);
-    }
-    
-    $count = mysqli_num_rows($answer);
-    if ($count == 0){
-    $_SESSION['polis_adi'] = $polis_adi;
-    }else{
-    $mesaj = "<h1> Hatalı Kullanıcı adı veya Şifre!</h1>";
-    }
-    }
-    if (isset($_SESSION['polis_adi'])){
-    header("Location: anaSite.php");
-    }
-?>
+   
+   if (isset($_POST['email']) and isset($_POST['password'])){ 
+   
+   extract($_POST); 
+   
     
 
-
+   $password = hash('sha256', $password); 
+   
+   $sql = "SELECT * FROM `polis` WHERE "; 
+   
+   $sql= $sql . "eposta='$email' and sifre='$password'"; 
+   
+    
+   
+   $answer = mysqli_query($connect, $sql); 
+   
+   if(!$answer ){ 
+   
+       echo '<br>Hata:' . mysqli_error($connect); 
+   
+   } 
+   
+   
+   $count = mysqli_num_rows($answer); 
+   
+   if ($count == 1){ 
+   
+       $_SESSION['email'] = $email; 
+   
+    }else{ 
+   
+   $msg = "<h1>  Kullanıcı adı veya Şifre Hatalıdır !</h1>"; 
+   
+    } 
+   
+   } 
+   
+   if (isset($_SESSION['email'])){ 
+   
+   header("Location: anaSite.php"); 
+   
+   }else{ 
+   
+   
+   ?> 
 <html>
    <head>
      <title>Login Page</title>
      <meta http-equiv="Content-Type" content="text/html;  
-    charset=UTF-8" />
+         charset=UTF-8" />
      <meta charset="utf-8">
      <meta name="viewport" content="width=device-width, initial-scale=1">
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
    </head>
-<body >
+<body  style =" background-image: url(3d-render-police-line-tape-against-defocussed-background.jpg); color: white; ">
+
+<div class="row justify-content-center">
+   <div class="col-lg-6 col-md-6 col-12">
     
     <form action="<?php $_PHP_SELF ?>" method="post" class="container" style="margin-top :50px; margin-bottom:50px">
-          <?php  
-            if(isset($mesaj)){ echo $mesaj;}  
             
-            ?>
-            <h2 style="color: grey; "> Login page  </h2><br> <br> 
+            <h2 >Police Login page  </h2><br> <br> 
+            <?php  
+            if(isset($msg)){ echo $msg;}  
+            
+            ?> 
        <div class="mb-3">
-          <label class="form-label">Username  </label>
-          <input type="text" class="form-control" name="polis_adi" >
+          <label class="form-label">Email   </label>
+          <input type="text" class="form-control" name="email"  required>
         </div>  
         <div class="mb-3">
            <label class="form-label">Password  </label>
-           <input name="password" type="password"   class="form-control" >
-        </div>  
+           <input name="password" type="password"   class="form-control" required >
+        </div>  <br>
 
        <div>
-          <button  type="submit" class="btn btn-success"> log in  </button>
+          <button  type="submit" class="btn btn-success"> log in  </button> &#160&#160&#160&#160 &#160&#160&#160&#160
+          <a href = "kayitForm.php" class="btn btn-info" >sign up </a> <br><br>
        </div><br>
      <div >
-         <a href = "kayit.php" >sign up </a> <br><br>
-         <a  href = "anaSite.php" class="link-secondary">Main Page</a>
-          <br/><br/>
-          
-          
-   
+       
      </form>
+   </div>
+   </div>
   </div> 
 </body>
 </html>
+<?php } ?>
