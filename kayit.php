ad955a3909766f8e666b50ebf0c700188ec9d72b
WAHIB MOQBEL <75446834+Wahib-eng@users.noreply.github.com>
2022-06-22 09:30:36 +0300

    Update kayit.php

--- kayit.php
+++ kayit.php
@@ -1,78 +1,41 @@
-<!DOCTYPE html>
-<?php
-    require ('mysqlConn.php');
-    if (isset($_POST['polis_adi']) && isset($_POST['sifre'])){
-         extract($_POST);
-        
-         $sifre = hash('sha256', $sifre);  	 	 	 	 		 	 	 	 	 	
-         $sql="INSERT INTO `polis` (kullaniciadi, eposta ,sifre , dt, adres, tn)";
-         $sql = $sql . " VALUES ('$polis_adi','$polis_eposta' ,'$sifre',  '$polis_dt' ,'$polis_adresi','$polis_tn')";
-        $answer = mysqli_query($connect, $sql);
-        if ($answer){
-            $msg = "<h1>Kullanıcı oluşturuldu.</h1>";
-        }
-        else{
-            $msg  = "<h1>Kullanıcı oluşturulamadı!</h1>";
-        }
-    }
-?>
-
 <html>
-   <head>
-     <title>Register Page </title>
-     <meta http-equiv="Content-Type" content="text/html;  
-    charset=UTF-8" />
-     <meta charset="utf-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1">
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-   
-   </head>
+<?php
+include ('MysqlConn.php');
+
+    $polis_adi=$_POST['polis_adi'];
+    $polis_eposta=$_POST['polis_eposta'];
+    $password=$_POST['password'];
+    $password2=$_POST['password2'];
+    $polis_tn=$_POST['polis_tn'];
+    $polis_dt=$_POST['polis_dt'];
+    $polis_adresi=$_POST['polis_adresi'];
+
   
-<body>  
+    $password = hash('sha256', $password);  
+    $password2 = hash('sha256', $password2);
 
-
-      <?php 
-                if (isset($msg)) echo $msg;
-       ?>
-        <form  class="container"><br><br> 
-        
-         <h2 style="color: grey; "> Register Page </h2> <br><br>
-            <div class="mb-3">
-                 <label class="form-label">Name</label>
-                 <input type="text" class="form-control"  name="polis_adi" >
-            </div>
-            <div class="mb-3">
-                <label  class="form-label">Date of Birth</label>
-                <input type="date" class="form-control"  name="polis_dt">
-            </div>
-            <div class="mb-3">
-                <label  class="form-label">Email</label>
-                <input type="email" class="form-control"  placeholder="username@gmail.com"  name="polis_eposta">
-            </div>
-            <div class="mb-3">
-                <label  class="form-label">Password</label>
-                <input type="password" class="form-control" name="sifre"><br>
-            </div>
-            <div class="mb-3">
-                <label  class="form-label">Phone No</label>
-            <input type="text" class="form-control"  placeholder="0533-002-22-22"  name="polis_tn">
-            </div>
-            <div class="mb-3">
-                <label class="form-label">Address</label>
-                <input type="text" class="form-control"  name="polis_adresi">
-            </div>
+    
+    $sq = mysqli_query($connect, "SELECT eposta FROM polis WHERE eposta = '".$_POST['polis_eposta']."'") ;
+    if(mysqli_num_rows($sq)) {
+            echo('This email is already being used');
+            echo"<br> <a href='kayitForm.php' >click to go back ... </a> ";  
+    }
+    else {
+    $q = "INSERT INTO polis ".
+       "(adi_soyadi,eposta,sifre,sifre2, tn, dt, adres) ".
+       "VALUES ('$polis_adi', '$polis_eposta', '$password', 
+       '$password2', '$polis_tn', '$polis_dt', '$polis_adresi' )";
+         
+   
+    $answer = mysqli_query($connect, $q);
+    
+        if ($answer){
+            header ("location:giris.php"); 
             
-            <div class="mb-3">
-                 <button type="submit" class="btn btn-success">register</button> <br><br>
-            </div>
-            <div> 
-                <a href="giris.php" >Click to login</a><br><br>
-               <a class="link-secondary" href ="anaSite.php">Go to main page </a><br><br>
-               
-            </div>
-            
-        </form>
+            }
+             
+    mysqli_close($connect); 
+}
 
-</body>
+?>
 </html>
